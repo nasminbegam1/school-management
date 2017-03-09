64065d0588b0f89b5c703e331730df07c2acfcfb
nasminbegam1 <begam.nasmin91@gmail.com>
2017-03-09 13:04:38 +0530

    nasmin new breadcamp

--- app/Http/breadcrumbs.php
+++ app/Http/breadcrumbs.php
@@ -1,6 +1,12 @@
 <?php
+Breadcrumbs::register('home', function($breadcrumbs)
+{
+    $breadcrumbs->push('<i class="iconfa-home"></i>', route('login'));
+});
 Breadcrumbs::register('screen_list', function($breadcrumbs)
 {
-    $breadcrumbs->push('Screen List', route('screen_list'));
+    $breadcrumbs->parent('home');
+    $breadcrumbs->push('Screen', 'javascript:void(0);');
+    $breadcrumbs->push('List', route('screen_list'));
 });
 ?>
--- resources/views/_partials/breadcrumbs.blade.php
+++ resources/views/_partials/breadcrumbs.blade.php
@@ -1,10 +1,11 @@
 @if ($breadcrumbs)
-    <ul class="breadcrumb">
+    <ul class="breadcrumbs">
         @foreach ($breadcrumbs as $breadcrumb)
             @if (!$breadcrumb->last)
-                <li><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
+                <li><a href="{{ $breadcrumb->url }}">
+                {!! $breadcrumb->title !!}</a><span class="separator"></span></li>
             @else
-                <li class="active">{{ $breadcrumb->title }}</li>
+                <li class="active">{!! $breadcrumb->title !!}</li>
             @endif
         @endforeach
     </ul>
