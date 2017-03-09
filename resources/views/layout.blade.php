827fed10ed4431107f37ffbd8c4481b012cd713e
nasminbegam1 <begam.nasmin91@gmail.com>
2017-03-09 13:50:49 +0530

    09032017_nasmin_2

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -46,24 +46,6 @@
         
         {!! Breadcrumbs::render() !!}
         
-        <!--<ul class="breadcrumbs">
-            <li><a href="dashboard.html"><i class="iconfa-home"></i></a> <span class="separator"></span></li>
-            <li><a href="forms.html">Forms</a> <span class="separator"></span></li>
-            <li>Form Styles</li>
-            
-            <li class="right">
-                <a href="" data-toggle="dropdown" class="dropdown-toggle"><i class="icon-tint"></i> Color Skins</a>
-                <ul class="dropdown-menu pull-right skin-color">
-                    <li><a href="default">Default</a></li>
-                    <li><a href="navyblue">Navy Blue</a></li>
-                    <li><a href="palegreen">Pale Green</a></li>
-                    <li><a href="red">Red</a></li>
-                    <li><a href="green">Green</a></li>
-                    <li><a href="brown">Brown</a></li>
-                </ul>
-            </li>
-        </ul>-->
-        
         @yield('pageheader')
         
         <div class="maincontent">
